3044ea3b812593a5c00a065985742d4a8f2052a8
jk9077 <betterman00@naver.com>
2026-09-23 17:52:34 +0900

    add test code to verify supply relationship permissions

--- test/DeliveryRegistry.t.sol
+++ test/DeliveryRegistry.t.sol
@@ -12,3 +12,42 @@ contract OtherCaller {
         registry.allowRoute(supplier, receiver);
     }
 }
+
+contract DeliveryRegistryTest {
+    function test_AdminCanAllowRoute() public {
+        DeliveryRegistry registry = new DeliveryRegistry();
+
+        address supplier = address(0x1001);
+        address receiver = address(0x1002);
+
+        registry.allowRoute(supplier, receiver);
+
+        require(
+            registry.allowedRoutes(supplier, receiver),
+            "Admin should be able to register route"
+        );
+    }
+
+    function test_NonAdminCannotAllowRoute() public {
+        DeliveryRegistry registry = new DeliveryRegistry();
+        OtherCaller other = new OtherCaller();
+
+        address supplier = address(0x1001);
+        address receiver = address(0x1002);
+        bool blocked = false;
+
+        try other.allowRoute(registry, supplier, receiver) {
+            blocked = false;
+        } catch Error(string memory reason) {
+            blocked =
+                keccak256(bytes(reason)) == keccak256(bytes("Only admin"));
+        }
+
+        require(blocked, "Non-admin should be blocked");
+
+        require(
+            !registry.allowedRoutes(supplier, receiver),
+            "Unauthorized route should not be registered"
+        );
+    }
+}
